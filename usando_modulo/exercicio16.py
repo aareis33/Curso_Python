94192406870545b358b862a1a3a8486b7553fdca
aareis33 <alexandreandrade074@gmail.com>
2026-08-26 20:33:54 -0300

    exercicio16

--- usando_modulo/exercicio16.py
+++ usando_modulo/exercicio16.py
@@ -1,4 +1,6 @@
-num = float(input('digite um numero? '))
+##from math import trunc
+##num = float(input('digite um numero? '))
+##print('o numero {}, sua parte inteira é {}.'.format(num,trunc(num)  ))
 
-
-print('o numero {}, sua parte inteira é {}.'.format(num,  ))
+num = float(input('digite seu numero: '))
+print('o valor digitado foi {}, e a sua porção inteira e {}'.format(num, int(num)))
