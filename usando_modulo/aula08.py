ac5b3fff0e207c3406b54613a75cb57bdd5cc80e
aareis33 <alexandreandrade074@gmail.com>
2026-08-25 21:59:50 -0300

    exercicio16

---
+++ usando_modulo/aula08.py
@@ -0,0 +1,11 @@
+from math import sqrt, floor
+
+num = int(input('digite um numero? '))
+
+raiz = sqrt(num)
+
+print('a raiz de {} e igual a {}'.format(num, floor (raiz)))
+
+## import emoji
+
+## print(emoji.emojize('ola :earth_americas:'.use_aliases=True))
---
+++ usando_modulo/exercicio16.py
@@ -0,0 +1,4 @@
+num = float(input('digite um numero? '))
+
+
+print('o numero {}, sua parte inteira é {}.'.format(num,  ))
