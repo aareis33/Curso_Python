d56e0e6fd0429069cd072aac1e1e913e50b669f5
aareis33 <alexandreandrade074@gmail.com>
2026-08-29 19:19:16 -0300

    testando maninupulações

---
+++ usando_modulo/maninupulando_texto.py
@@ -0,0 +1,27 @@
+## fatiamento 
+
+frase = 'curso em video python'
+frase[12:6:74]
+frase.count('o',0, 13)
+frase.find('deo')
+frase.find('android')
+
+## transformação
+
+frase.replace('puthon', 'android')
+frase.upper()
+frase.lower()
+frase.capitalize()
+frase.title()
+frase.split()
+frase.rsplit()
+
+
+## divisao
+
+frase.split()
+
+'-'.join(frase)
+
+frase = 'curso do alexandre' 
+print(frase.lower().find('curso'))
