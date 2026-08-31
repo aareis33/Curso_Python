ed67a3cc5b46bc21bcb3aab02f25cccdbde03637
aareis33 <alexandreandrade074@gmail.com>
2026-08-30 22:37:28 -0300

    exercicio 22

--- usando_modulo/exercicio22.py
+++ usando_modulo/exercicio22.py
@@ -3,3 +3,12 @@
 ##O nome com todas minúsculas.
 ##Quantas letras ao todo (sem considerar espaços).
 ##Quantas letras tem o primeiro nome.
+
+nome = str(input('Digite seu nome completo: ')).strip()
+print('analisando seu nome...')
+print('seu nome em maiuscukas é {}'.format(nome.upper()))
+print('seu nome em minusculas é {}'.format(nome.lower()))
+print('seu nome tem ao todo {} letras'.format(len(nome) - nome.count(' ')))
+##nome.find('seu primeiro nome tem {} letras'.format(nome.find(' ')))
+separa = nome.split()
+print('seu primeiro nome e {} e tem tantas letras {}'.format(separa[0], len(separa[0])))
