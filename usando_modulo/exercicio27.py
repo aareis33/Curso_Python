12dcd6b6682b503abe4fe816688afaa0a0819c6e
aareis33 <alexandreandrade074@gmail.com>
2026-08-31 22:57:39 -0300

    exercicio27

--- usando_modulo/exercicio27.py
+++ usando_modulo/exercicio27.py
@@ -2,4 +2,12 @@
 ##Faça um programa que leia o nome completo de uma pessoa, mostrando em seguida o primeiro e o último nome separadamente.
 ##Ex: Ana Maria de Souza
 ##primeiro = Ana
-##último = Souza
+##último = Souza]
+
+
+n = str(input('digite seu nome completo: ')).strip()
+nome = n.split()
+
+print('muito prazer em te conhecer')
+print('seu primeiro nome e {}'.format(nome[0]))
+print('seu primeiro nome e {}'.format(nome[len(nome)-1]))
