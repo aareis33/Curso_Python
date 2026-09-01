613c460c712f5d13b116cee4c1c5945c5c4bb0a7
aareis33 <alexandreandrade074@gmail.com>
2026-08-31 22:17:55 -0300

    exercicio24[

--- usando_modulo/exercicio24.py
+++ usando_modulo/exercicio24.py
@@ -1,1 +1,4 @@
 ##💡 Crie um programa que leia o nome de uma cidade e diga se ela começa ou não com o nome "SANTO".
+
+cid = str(input('em que cidade voce nasceu? ')).strip()
+print(cid[:5].upper() == 'Santo')
