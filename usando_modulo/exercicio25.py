bfef2cb8326a40ebaf36fba6dd496cce7375cca6
aareis33 <alexandreandrade074@gmail.com>
2026-08-31 22:38:52 -0300

    exercicio25

--- usando_modulo/exercicio25.py
+++ usando_modulo/exercicio25.py
@@ -1,1 +1,3 @@
 ## Crie um programa que leia o nome de uma pessoa e diga se ela tem "SILVA" no nome.
+nome = str(input('qual e seu nome completo? ')).strip()
+print("seu nome tem silva? {}".format('Silva' in nome.lower()))
