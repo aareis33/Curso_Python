e208e5c37f1de73362922a4b2f80f0422c633c7e
aareis33 <alexandreandrade074@gmail.com>
2026-09-01 19:09:06 -0300

    condições

---
+++ condicoes/condicoes.py
@@ -0,0 +1,28 @@
+## condição
+##tempo = int(input('quantos anos tem seu carro? '))
+
+##if tempo <=3:
+## bloco True
+    ##print('carro novo')
+
+##else:
+    ##print('carro velho')
+##print('--FIM--')
+## bloco false
+
+##nome = str(input('qual seu nome? '))
+##if nome == ('gustavo'):
+    ##print('que nome lindo voce tem ')
+##else:
+    ##print('seu nome e tao normal ')
+##print('bom dia, {}'.format(nome))
+
+n1 = float(input('nota 1 '))
+n2 = float(input('nota 2'))
+m = (n1 + n2)/2
+
+print('sua media foi {:1f}'.format(m))
+if m >= 6.00:
+    print('sua media foi boa')
+else:
+    print('sua media foi ruim')
