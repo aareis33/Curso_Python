313d7df1c94e7474fbadf100a70c76bc05c80543
aareis33 <alexandreandrade074@gmail.com>
2026-08-31 22:05:14 -0300

    exercicio 23

--- usando_modulo/exercicio23.py
+++ usando_modulo/exercicio23.py
@@ -5,3 +5,17 @@
 ##dezena: 3
 ##centena: 8
 ##milhar: 1
+
+num = int(input('informe um numero: '))
+## n = str(num)
+u = num // 1 % 10
+d = num // 10 % 10
+c = num // 100 % 10 
+m = num // 1000 % 10
+
+
+print('analisando o numero {}'.format(num))
+print('unidade {}'.format(u))
+print('dezena {}'.format(d))
+print('centena {}'.format(c))
+print('milha {}'.format(m))
